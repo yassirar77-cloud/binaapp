35730035435f7bce6ce9e0f148bca145bed8575f
yassirar77-cloud <yassirar77@gmail.com>
2025-12-15 09:04:44 +0800

    Fix webpack alias for @

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
 
   typescript: {
     ignoreBuildErrors: true
